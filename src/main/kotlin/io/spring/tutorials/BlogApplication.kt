a1070f7b4b7091ba162ae97e8a54f697fe7734e1
villiseck <villiseck@abes.fr>
2023-09-15 20:09:26 +0200

    Entities creation + main correction

--- src/main/kotlin/io/spring/tutorials/BlogApplication.kt
+++ src/main/kotlin/io/spring/tutorials/BlogApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BlogApplication
+open class BlogApplication
 
 fun main(args: Array<String>) {
     runApplication<BlogApplication>(*args) {
