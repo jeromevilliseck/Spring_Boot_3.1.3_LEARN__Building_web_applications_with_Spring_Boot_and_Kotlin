4c00fa70d648e836dc83e22d1110e61343ced61d
villiseck <villiseck@abes.fr>
2023-09-15 19:02:17 +0200

    Test instance lifecycle - @BeforeAll and @AfterAll

--- src/test/kotlin/io/spring/tutorials/IntegrationTests.kt
+++ src/test/kotlin/io/spring/tutorials/IntegrationTests.kt
@@ -1,6 +1,8 @@
 package io.spring.tutorials
 
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -10,10 +12,22 @@ import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
     @Test
     fun `Assert blog page title, content and status code`() {
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(entity.body).contains("<h1>Blog</h1>")
+    }
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+    @AfterAll
+    fun teardown() {
+        println(">> Tear down")
     }
 }
