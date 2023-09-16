e02f20cd228df57de459d8cdf5c66aaccc25fe2a
villiseck <villiseck@abes.fr>
2023-09-16 11:08:27 +0200

    Writing repository layer

---
+++ src/main/kotlin/io/spring/tutorials/repositories/Repositories.kt
@@ -0,0 +1,14 @@
+package io.spring.tutorials.repositories
+
+import io.spring.tutorials.entities.Article
+import io.spring.tutorials.entities.User
+import org.springframework.data.repository.CrudRepository
+
+interface ArticleRepository : CrudRepository<Article, Long> {
+    fun findBySlug(slug: String): Article?
+    fun findAllByOrderByAddedAtDesc(): Iterable<Article>
+}
+
+interface UserRepository : CrudRepository<User, Long> {
+    fun findByLogin(login: String): User?
+}
